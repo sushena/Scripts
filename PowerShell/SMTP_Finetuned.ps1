2d378b74dc4152e8c1f93e446c4596b387b65329
Sushena Parthasarathy <psushena@sentient-energy.com>
2019-05-30 15:52:38 +0530

    Added Eaton PS script

---
+++ PowerShell/SMTP_Finetuned.ps1
@@ -0,0 +1,59 @@
+﻿param(  [String] $remoteHost="mail.eaton.com", 
+        [String] $domain="smtp.gmail.com", 
+        [String] $sendingdomain="smtp.gmail.com" 
+     )
+
+function readResponse 
+{
+    while($stream.DataAvailable)  
+    {  
+        $read = $stream.Read($buffer, 0, 1024)    
+        #Write-Host -n -ForegroundColor DarkYellow ($encoding.GetString($buffer, 0,$read))
+        ""
+        $Data = ($encoding.GetString($buffer, 0,$read))
+        #$dt=[int]$Data.split(" ")[0]
+        #$dt.GetType()
+        if (( [int]$Data.split(" ")[0] -gt 200 ) -and ( [int]$Data.split(" ")[0] -lt 299 ))
+        {
+            Write-Host -n -ForegroundColor Cyan "[OK] -->"$Data.split(" ")[0]
+            #Write-EventLog -LogName Application -EntryType Information -EventId 8887 -Source {} -Message "No problem detected with SMTP connection"
+
+        }
+        elseif (( [int]$Data.split(" ")[0] -ge 400 ) -and ( [int]$Data.split(" ")[0] -lt 499 ))
+        {
+            Write-Host -n -ForegroundColor Cyan "[ERROR] -->"$Data.split(" ")[0]
+            #Write-EventLog -LogName Application -EntryType Error -EventId 8886 -Source {} -Message "Connectivity ERROR with SMTP"
+        }
+    } 
+}
+
+$port = 25 
+$socket = new-object System.Net.Sockets.TcpClient($remoteHost, $port) 
+if($socket -eq $null) 
+{ 
+    return; 
+} 
+
+$stream = $socket.GetStream() 
+$writer = new-object System.IO.StreamWriter($stream) 
+$buffer = new-object System.Byte[] 1024 
+$encoding = new-object System.Text.AsciiEncoding 
+
+readResponse($stream)
+$command = "HELO "+ $domain 
+write-host -ForegroundColor Green $command
+""
+
+$writer.WriteLine($command) 
+$writer.Flush()
+start-sleep -m 500 
+readResponse($stream)
+Write-Host -ForegroundColor DarkRed ""
+$command = "MAIL FROM: <smtpcheck@" + $sendingdomain + ">" 
+write-host -foregroundcolor DarkGreen $command
+$writer.WriteLine($command) 
+$writer.Flush()
+start-sleep -m 500 
+readResponse($stream)
+$writer.Close() 
+$stream.Close() 
--- PowerShell/win_agent_log_GCP_installation.ps1
+++ PowerShell/win_agent_log_GCP_installation.ps1
@@ -3,6 +3,11 @@ $ErrorActionPreference = "SilentlyContinue"
 md C:\Users\logileprod\GCP_Stackdriver\
 cd C:\Users\logileprod\GCP_Stackdriver\
 
+$StackMonitoring = "StackdriverMonitoring"
+$StackLogging = "StackdriverLogging"
+
+if (( Get-Service 
+
 Invoke-webrequest https://repo.stackdriver.com/windows/StackdriverMonitoring-GCM-46.exe -OutFile StackdriverMonitoring-GCM-46.exe;
 
 ECHO 'Y' | ./StackdriverMonitoring-GCM-46.exe /FORCE /S
@@ -10,3 +15,9 @@ ECHO 'Y' | ./StackdriverMonitoring-GCM-46.exe /FORCE /S
 Invoke-WebRequest https://dl.google.com/cloudagents/windows/StackdriverLogging-v1-8.exe -OutFile StackdriverLogging-v1-8.exe;
 
 ECHO 'Y' | ./StackdriverLogging-v1-8.exe /FORCE /S
+
+Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.201 -Force
+
+Install-Module -Name Googlecloud -AllowClobber -Confirm:$False -Force
+
+set $env:GCLOUD_SDK_INSTALLATION_NO_PROMPT
